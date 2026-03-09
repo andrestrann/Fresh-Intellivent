4b7092f7b2469a910ad67b676d6d99155c6d85a9
andrestrann <andre.strann@gmail.com>
2026-03-09 22:07:28 +0100

    Added Action Flow Card
    Added an Action Flow Card that enables constant speed mode at the RPM selected in the flow

--- app.js
+++ app.js
@@ -3,6 +3,26 @@ const Homey = require('homey');
 class FreshIntelliventApp extends Homey.App {
   onInit() {
     this.log('Fresh Intellivent App is running...');
+
+
+    // Action Flow Cards //
+
+    // Activate Constant speed at selected RPM
+    const actionCard = this.homey.flow.getActionCard('start-constant-speed-rpm');
+
+    actionCard.registerRunListener(async (args, state) => {
+      try {
+        let rpm = Number(args.rpm);
+        if (!Number.isFinite(rpm)) {
+          rpm = 2400
+        }
+        await args.device.startConstantSpeedRPM(rpm);
+        return true; // indicates success to Flow
+      } catch (err) {
+        this.error('Failed to activate constant speed mode:', err);
+        throw new Error(this.homey.__('errors.activation_failed') || 'Activation failed');
+      }
+    });
   }
 }
 
